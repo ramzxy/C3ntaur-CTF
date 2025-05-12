b50272102d43e0a36358ebadd03b99901a6d13c4
Asynchronous <198262893+asynchronous-x@users.noreply.github.com>
2025-05-12 16:47:25 -0600

    fix spelling error

--- instrumentation.ts
+++ instrumentation.ts
@@ -3,7 +3,7 @@ import { ChallengeIngestionService } from "@/lib/challenge-ingestion";
 export async function register() {
   console.log('Running startup tasks...');
 
-  if (process.env.INGEST_CHALLENGES_AT_STARUP === 'true') {
+  if (process.env.INGEST_CHALLENGES_AT_STARTUP === 'true') {
     // Run challenge ingestion
     const challengeIngestion = new ChallengeIngestionService();
     await challengeIngestion.ingestChallenges();
